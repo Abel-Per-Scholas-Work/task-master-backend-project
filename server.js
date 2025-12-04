eebbc4b17e6605f8a86cdf9a33284cffe34cf44a
belunatic <abelluvyou@yahoo.com>
2025-12-04 11:45:05 -0800

    move a line up

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
-const mongoose = require("mongoose");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 // Set up all variables in the .env file
 require("dotenv").config();
@@ -19,8 +19,8 @@ const app = express();
 // ========= Middlewares =================
 app.use(morgan("dev")); // logger
 app.use(express.json()); // body parser
-require("./config/passport");
 app.use(cors({ origin: "http://localhost:5173" }));
+require("./config/passport");
 
 // ========= Routes ======================
 app.use("/api/users", require("./routes/userRoutes"));
