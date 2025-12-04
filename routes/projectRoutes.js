7a207d520cd9aa828a446de958419564a205c110
belunatic <abelluvyou@yahoo.com>
2025-12-03 16:18:38 -0800

    update text to project

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -25,12 +25,12 @@ router.get("/:id", async (req, res) => {
 	// the owner is only one with permission to view it.
 	try {
 		// This needs an authorization check
-		//get to be update note
+		//get to be update project
 		const project = await Project.findById(req.params.id);
 		if (!project) {
 			return res.status(400).json({ message: "Invalid ID" });
 		}
-		// check if the user field on that note matches the authenticated user’s _id.
+		// check if the user field on that project matches the authenticated user’s _id.
 		if (project.user.toString() !== req.user._id) {
 			return res
 				.status(403)
@@ -43,68 +43,72 @@ router.get("/:id", async (req, res) => {
 	}
 });
 
-// POST /api/projects - Create a new note
+// POST /api/projects - Create a new project
 router.post("/", async (req, res) => {
 	try {
-		const note = await Project.create({
+		const project = await Project.create({
 			...req.body,
 			// The user ID needs to be added here
 			user: req.user._id,
 		});
-		res.status(201).json(note);
+		res.status(201).json(project);
 	} catch (err) {
 		res.status(400).json(err);
 	}
 });
 
-// PUT /api/projects/:id - Update a note
+// PUT /api/projects/:id - Update a project
 router.put("/:id", async (req, res) => {
 	try {
 		// This needs an authorization check
-		//get to be update note
+		//get to be update project
 		const getUpdateProject = await Project.findById(req.params.id);
 		if (!getUpdateProject) {
 			return res.status(400).json({ message: "Invalid ID" });
 		}
-		// check if the user field on that note matches the authenticated user’s _id.
+		// check if the user field on that project matches the authenticated user’s _id.
 		if (getDeleteProject.user.toString() !== req.user._id) {
 			return res
 				.status(403)
-				.json({ message: "Not Authorize to update this note" });
+				.json({ message: "Not Authorize to update this project" });
 		}
 
-		const note = await Project.findByIdAndUpdate(req.params.id, req.body, {
+		const project = await Project.findByIdAndUpdate(req.params.id, req.body, {
 			new: true,
 		});
-		if (!note) {
-			return res.status(404).json({ message: "No note found with this id!" });
+		if (!project) {
+			return res
+				.status(404)
+				.json({ message: "No project found with this id!" });
 		}
-		res.json(note);
+		res.json(project);
 	} catch (err) {
 		res.status(500).json(err);
 	}
 });
 
-// DELETE /api/projects/:id - Delete a note
+// DELETE /api/projects/:id - Delete a project
 router.delete("/:id", async (req, res) => {
 	try {
 		// This needs an authorization check
-		//get to be update note
+		//get to be update project
 		const getDeleteProject = await Project.findById(req.params.id);
 		if (!getDeleteProject) {
 			return res.status(400).json({ message: "Invalid ID" });
 		}
-		// check if the user field on that note matches the authenticated user’s _id.
+		// check if the user field on that project matches the authenticated user’s _id.
 		if (getDeleteProject.user.toString() !== req.user._id) {
 			return res
 				.status(403)
-				.json({ message: "Not Authorize to delete this note" });
+				.json({ message: "Not Authorize to delete this project" });
 		}
-		const note = await Project.findByIdAndDelete(req.params.id);
-		if (!note) {
-			return res.status(404).json({ message: "No note found with this id!" });
+		const project = await Project.findByIdAndDelete(req.params.id);
+		if (!project) {
+			return res
+				.status(404)
+				.json({ message: "No project found with this id!" });
 		}
-		res.json({ message: "Note deleted!" });
+		res.json({ message: "project deleted!" });
 	} catch (err) {
 		res.status(500).json(err);
 	}
